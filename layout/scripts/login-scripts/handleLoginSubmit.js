d67660645d2428595bd6c6ae9c90f199f61e0ab2
cstrayer24 <cstrayer4303@gmail.com>
2024-01-09 12:09:31 -0500

    login-stuff

---
+++ layout/scripts/login-scripts/handleLoginSubmit.js
@@ -0,0 +1,21 @@
+import apiUrl from "../apiUrl.js";
+import formDataToObj from "../formDataToObj.js";
+
+const form = document.querySelector("#main_form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const rawFormData = new FormData(form);
+  const bodyObj = formDataToObj(rawFormData);
+  const req = await fetch(`${apiUrl}/user-login`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(bodyObj),
+  });
+
+  if (req.ok) {
+    //TODO pipe authenticated user into app
+  }
+});
